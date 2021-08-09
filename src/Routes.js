e99190cda55fb6639beac83e423c2c1674e387f7
Gabriel Nicolitoiu <nicolitoiu.gabriel23@gmail.com>
2021-08-09 19:29:43 +0100

    deploy to github pages

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import App from "./App";
 import Store from "./Store";
 
 
 const Routes = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
       <Route exact path="/" component={App} />
       <Route exact path="/store" component={Store} />
       
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
