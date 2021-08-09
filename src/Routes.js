f971e36b23348cbaf4714756c1c60f06a925094f
Gabriel Nicolitoiu <nicolitoiu.gabriel23@gmail.com>
2021-08-09 18:28:17 +0100

    Routes change

--- src/Routes.js
+++ src/Routes.js
@@ -6,7 +6,7 @@ import Store from "./Store";
 
 const Routes = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/shop">
       <Switch>
       <Route exact path="/" component={App} />
       <Route exact path="/store" component={Store} />
